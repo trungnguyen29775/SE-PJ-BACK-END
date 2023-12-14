9eceb9f6c568a01448c0b500710a4922df69615c
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2023-12-14 17:04:07 +0700

    14/12/2023: Add database initation

--- src/index.js
+++ src/index.js
@@ -2,15 +2,14 @@ const express  = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const cors = require('cors')
-
-
-
 const port = 5000
 
 app.use(cors())
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
+
+db.sequelize.sync({alter:true})
 
 
 app.listen(port,()=>
