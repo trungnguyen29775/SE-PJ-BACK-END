e4bcacc8ba3eaa392bcb222d2dfa2b74ee2e1b36
trungnguyen29775 <ITITIU20332@student.hcmiu.edu.vn>
2023-12-26 11:07:31 +0700

    26/12/2023: Sign up and sign in

--- src/service/user.service.js
+++ src/service/user.service.js
@@ -9,7 +9,12 @@ exports.login = async (req, res) => {
     const checkName = await User.findOne({ where: { email: user.email } });
     if (checkName) {
         if (user.password === checkName.password) {
-            res.status(200).send('Authoried');
+            const responseDate = {
+                name: checkName.name,
+                email: checkName.email,
+                userId: checkName.user_id,
+            };
+            res.status(200).send(responseDate);
         }
     } else {
         res.send(500)("User name doesn't exist.");
@@ -25,26 +30,19 @@ exports.signup = async (req, res) => {
             isAdmin: false,
         };
 
-        const responseData = {
-            name: newUser.name,
-            email: newUser.email,
-            isAdmin: newUser.isAdmin,
-        };
-        res.status(200).send(responseData);
-
-        // console.log(newUser);
-        // const checkName = await User.findOne({ where: { email: newUser.email } });
-        // if (checkName) {
-        //     res.send('The email already exist.');
-        // } else {
-        //     await User.create(newUser);
-        //     const responseData = {
-        //         name: newUser.name,
-        //         email: newUser.email,
-        //         isAdmin: newUser.isAdmin,
-        //     };
-        //     res.send(responseData);
-        // }
+        console.log(newUser);
+        const checkName = await User.findOne({ where: { email: newUser.email } });
+        if (checkName) {
+            res.send('The email already exist.');
+        } else {
+            await User.create(newUser);
+            const responseData = {
+                name: newUser.name,
+                email: newUser.email,
+                isAdmin: newUser.isAdmin,
+            };
+            res.send(responseData);
+        }
     } catch (err) {
         res.status(500).send(`Error due to ${err}`);
     }
